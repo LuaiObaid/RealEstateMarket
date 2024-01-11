a7bbbad55d7c4bcbf1f8a97ad104dcc1b1f8dcbc
Luai Obaid <Luai.Obaid@knightec.se>
2024-01-11 11:55:46 +0100

    delete user

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { current } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice.js";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice.js";
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
 import {app} from '../firebase.js'
 import { useDispatch } from "react-redux";
@@ -71,6 +71,22 @@ export default function Profile() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
+  const handleDeleteUser = async()=>{
+       try {
+          dispatch(deleteUserStart());
+          const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+            method: 'DELETE',
+          });
+          const data = await res.json();
+          if(data.success === false){
+            dispatch(deleteUserFailure(data.message))
+            return
+          }
+          dispatch(deleteUserSuccess(data))
+       } catch (error) {
+        dispatch(deleteUserFailure(error.message))
+       }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -123,7 +139,7 @@ export default function Profile() {
         p-3 uppercase hover:opacity-95 disabled:opacity-80" onClick={handleSubmit}>{loading? '...loading':'Update'}</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer" onClick={handleDeleteUser}>Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-7">{error? error: ""}</p>
