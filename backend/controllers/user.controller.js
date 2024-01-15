a82320dfdd001c94f9ae88f9027798e2e6491275
Luai Obaid <Luai.Obaid@knightec.se>
2024-01-15 14:48:47 +0100

    get user listing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import Listing from "../models/listing.model.js"
 import { errorHandler } from "../utilis/error.js"
 import brcyptjs from 'bcryptjs'
 export const test = (req, res)=>{
@@ -38,4 +39,19 @@ export const deleteUser = async (req, res, next)=>{
         next(error)
         
     }
+}
+
+export const getUserListing = async(req, res, next)=>{
+    if(req.user.id === req.params.id){
+        try {
+            const listing = await Listing.find({userRef: req.params.id})
+            res.status(200).json(listing)
+        } catch (error) {
+            next(error)
+        }
+
+    }else{
+        return next(errorHandler(401, 'you can only view your lsiting!'))
+    }
+
 }
