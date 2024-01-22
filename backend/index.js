69f654552ae462fc777768e85d7c5cdc91e072c2
Luai Obaid <Luai.Obaid@knightec.se>
2024-01-22 14:20:28 +0100

    add preinsatllation

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,12 @@ app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
 
 
-app.use(path.join(__dirname, '/client/dist'));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statuCode || 500;
